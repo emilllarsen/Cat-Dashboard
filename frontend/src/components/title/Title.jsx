c12a44872432ce3a4c062e93221ed88a1441c7e7
Emil Larsen <emilvl@stud.ntnu.no>
2026-07-30 16:45:39 +0200

    CRUD operations on cats inside services, and created soem on title. Hamburger file is created

--- frontend/src/components/title/Title.jsx
+++ frontend/src/components/title/Title.jsx
@@ -1,12 +1,19 @@
 import styles from  "./Title.module.css";
-
+import { useState } from "react";
 export function Title({ day, date, month }) {
+const [ openNewCatForm, setOpenNewCatForm ] = useState(false);
+const [ openNewChore, setOpenNewChore ] = useState(false);
+const [ openLogWeight, setOpenLogWeight ] = useState(false);
+
   return (
     <section className={styles.titleWrapper}>
       <h1 className={styles.mainHeading}>The Cat House</h1>
       <span className={styles.datesInfo}>
         {day}, {date} {month}
       </span>
+
+      <div className={styles.buttons}>
+      </div>
     </section>
   );
 }
