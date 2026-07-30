a822e0bc0df14b3ef03b2d50847b588e3a97114e
Emil Larsen <emilvl@stud.ntnu.no>
2026-07-30 16:06:01 +0200

    generic api fetcher

---
+++ frontend/src/api.js
@@ -0,0 +1,31 @@
+const {
+  VITE_API_PROTOCOL,
+  VITE_API_HOSTNAME,
+  VITE_API_VERSION,
+  VITE_API_PORT,
+} = import.meta.env;
+
+const API_URL = `${VITE_API_PROTOCOL}://${VITE_API_HOSTNAME}:${VITE_API_PORT}/api/${VITE_API_VERSION}`;
+
+export async function apiFetch(endpoint, options = {}) {
+  const headers = {
+    ...(options?.headers || {}),
+    "Content-Type": "application/json",
+  };
+  const response = await fetch(API_URL, endpoint, {
+    ...options,
+    headers,
+  });
+  const result = await response.json();
+
+  if (!result) {
+    throw new Error(
+      result?.error ||
+        result?.err ||
+        result?.msg ||
+        result?.message ||
+        "An error has occured....",
+    );
+  }
+  return result;
+}
